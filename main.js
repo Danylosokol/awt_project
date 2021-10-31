f078402e7ddb2feaacca71687e95c38758e29c2f
Danylo Sokol <dansokol677@gmail.com>
2021-10-31 15:53:50 +0100

    finished comment section and fix main page

--- main.js
+++ main.js
@@ -4,15 +4,24 @@ window.onload = function () {
 
 let nextSectionButton = document.querySelector(".nextSection");
 let articles = document.getElementById("nameOfSection");
+let comments = document.getElementById("comments");
 let content;
 
 if (nextSectionButton !== null) {
     document.addEventListener('scroll', function () {
-        if (window.scrollY >= articles.getBoundingClientRect().top) {
+        if (window.scrollY >= articles.getBoundingClientRect().top &&
+            window.scrollY < comments.getBoundingClientRect().top &&
+            nextSectionButton.textContent === "\u22C1") {
+            nextSectionButton.href="#comments"
+        } else if (window.scrollY >= articles.getBoundingClientRect().top &&
+            window.scrollY < comments.getBoundingClientRect().top &&
+            nextSectionButton.textContent === "\u22C0") {
+            nextSectionButton.href="#top"
+        }else if(window.scrollY >= comments.getBoundingClientRect().top){
             content = "\u22C0";
             nextSectionButton.textContent = content;
-            nextSectionButton.href = "#top";
-        } else {
+            nextSectionButton.href = "#nameOfSection";
+        } else{
             content = "\u22C1";
             nextSectionButton.textContent = content;
             nextSectionButton.href = "#nameOfSection";
